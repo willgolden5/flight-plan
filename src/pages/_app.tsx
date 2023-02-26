99d0b4b8bd859ca90a059134b503b19033f81d37
William Golden <wtgolden5@gmail.com>
2023-02-25 22:06:04 -0500

    run prisma push and add chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { type AppType } from 'next/app';
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
 
-import { api } from "chakra-ui/utils/api";
+import { api } from 'chakra-ui/utils/api';
 
-import "chakra-ui/styles/globals.css";
+import 'chakra-ui/styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ChakraProvider>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ChakraProvider>
   );
 };
 
