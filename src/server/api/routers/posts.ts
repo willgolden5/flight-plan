8500d140ff3b8400906a357d9345fab58ae4a3cc
William Golden <wtgolden5@gmail.com>
2023-03-04 17:48:23 -0500

    boilerplate post feed

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+} from "flight-plan/server/api/trpc";
+
+
+export const postsRouter = createTRPCRouter({
+  all: protectedProcedure
+    .input(z.object({ email: z.string(), first: z.string(), last: z.string() }))
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    .query(({ input, ctx }) => ctx.prisma.alpha.upsert({
+      create: {
+        email: input.email,
+        first: input.first,
+        last: input.last,
+      },
+      where: { email: input.email },
+      update: {
+        first: input.first,
+        last: input.last,
+      }
+    })),
+
+});
