1bc6b65cc7e9d670d57e6939d34f9a841e78ecd9
William Golden <wtgolden5@gmail.com>
2023-03-11 14:51:50 -0500

    Fixed bad toast

---
+++ src/pages/api/airmen/auth.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { last, certNum, userId } = req.body
+    const authResult = await fetch(`https://airmen-auth-service.fly.dev/pilot?lastName=${last}&certificate=${certNum}&id=${userId}`)
+    
+    if(authResult.status === 401) {
+        res.status(401).json({ message: 'Unauthorized' })
+    }
+    if(authResult.status === 404) {
+        res.status(404).json({ message: 'Pilot Not Found' })
+    }
+    if(authResult.status === 500) {
+        res.status(500).json({ message: 'Internal Server Error' })
+    }
+    
+    res.status(200).json({ message: 'Pilot Validated' })
+}
